05526de04c92024645ff500afa1dfaf3be54a9a2
toffee <antoinette.zu@icloud.com>
2022-05-12 23:38:17 +0300

    auth instance added

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,34 @@
+/* eslint-disable */
 import axios from 'axios';
 
 const instance = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL,
     withCredentials: true,
+    headers: {},
+});
+let axiosCredentialInterceptorsId: number;
+
+export const updateAxiosClientCredential = (accessToken: string) => {
+    try {
+        instance.interceptors.request.eject(axiosCredentialInterceptorsId);
+    } catch (error) {
+        console.error('at axios in updateAxiosClientCredential', error);
+    }
+
+    axiosCredentialInterceptorsId = instance.interceptors.request.use((config) => {
+        if (config.headers) {
+            config.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
+        return config;
+    });
+};
+
+instance.interceptors.response.use((res) => {
+    const { accessToken } = res.data;
+    if (accessToken) {
+        updateAxiosClientCredential(accessToken);
+    }
+    return res;
 });
 
 export default instance;
--- src/api/routes/apiRoutes.ts
+++
@@ -1,7 +0,0 @@
-export interface Route {
-    (...pathSegments: string[]): string;
-  }
-
-  export type APIRoutes<T> = {
-    [K in keyof T]: Route;
-  };
